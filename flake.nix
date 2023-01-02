bba37444587807aff6236b9ef4aecf169ba88e8f
Pedro Souza <pedrohms412@gmail.com>
2023-01-02 10:24:29 -0300

    Some tests

--- flake.nix
+++ flake.nix
@@ -8,13 +8,41 @@
       (system:
         let pkgs = nixpkgs.legacyPackages.${system}; in
         {
-          devShells.default = pkgs.mkShell {
-            name = "nix";
-            buildInputs = with pkgs; [
-
-            ];
-            shellHook = "";
-          } ;
+          devShells = {
+            default = pkgs.mkShell {
+              name = "nix";
+              buildInputs = with pkgs; [
+                nodejs
+                go
+                python3
+                sumneko-lua-language-server
+                maven
+                jdk  
+              ];
+              shellHook = "";
+            };
+            python2 = pkgs.mkShell {
+              name = "nix2";
+              buildInputs = with pkgs; [
+                python2
+              ];
+              shellHook = "";
+            };
+            php = pkgs.mkShell {
+              name = "php";
+              buildInputs = with pkgs; [
+                php81Packages.composer 
+                php81
+                ripgrep
+                fish
+                nodejs-18_x
+                php81Extensions.pdo_mysql
+                php81Extensions.pdo_sqlite
+              ];
+              shellHook = "";
+            }
+          };
         }
       );
 }
+
